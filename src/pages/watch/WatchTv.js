d7636a8b03ca629dfe2d9e1624e3ad90353d13ae
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2022-12-07 13:43:56 +0700

    Update streaming api

--- src/pages/watch/WatchTv.js
+++ src/pages/watch/WatchTv.js
@@ -67,7 +67,7 @@ const WatchTv = () => {
             <div className="container">
                 <div ref={videoRef} className="video-wrapper">
                     <iframe
-                        src={`${process.env.REACT_APP_TV_STREAMING_API_ENDPOINT}?tmdb=${id}&sea=${season}&epi=${esp}`}
+                        src={`${process.env.REACT_APP_TV_STREAMING_API_ENDPOINT}?id=${id}&s=${season}&e=${esp}`}
                         frameBorder="0"
                         title="Movie Player"
                         allowFullScreen
